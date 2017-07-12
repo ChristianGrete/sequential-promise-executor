a09b33c7a7d96b4a7b78a15bb9aa131e115b362c
Christian Grete <webmaster@christiangrete.com>
2017-07-12 14:55:53 +0200

    test(src): update spec

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,17 +1,65 @@
 import * as index from '.'
 
-// const SequentialPromiseExecutor = index.SequentialPromiseExecutor
+const indexHasOwnProperty = {}.hasOwnProperty.bind(index)
+const SequentialPromiseExecutor = index.SequentialPromiseExecutor
 
-// let sequentialPromiseExecutor
+var subject
 
 describe('SequentialPromiseExecutor', () => {
-  it('provides a default export', () => {
-    expect(index.hasOwnProperty('default')).toBe(true)
+  describe('when imported', () => {
+    it('is available as default member', () => {
+      expect(indexHasOwnProperty('default')).toBe(true)
+    })
+
+    it('is also available as named member', () => {
+      expect(indexHasOwnProperty('SequentialPromiseExecutor')).toBe(true)
+    })
   })
 
-  it('provides a named export', () => {
-    expect(index.hasOwnProperty('SequentialPromiseExecutor')).toBe(true)
+  describe('.constructor(...$factories)', () => {
+    const _prototype = SequentialPromiseExecutor.prototype
+    const _originalQueueMethod = _prototype.queue
+
+    beforeEach(() => {
+      _prototype.queue = jest.fn()
+    })
+
+    describe('when invoked with arguments', () => {
+      beforeEach(() => {
+        subject = new SequentialPromiseExecutor(
+          () => {},
+          () => {}
+        )
+      })
+
+      it('calls .queue(...$factories) and passes arguments', () => {
+        expect(subject.queue).toHaveBeenCalledWith(
+          expect.any(Function),
+          expect.any(Function)
+        )
+      })
+    })
+
+    describe('when invoked without arguments', () => {
+      beforeEach(() => {
+        subject = new SequentialPromiseExecutor()
+      })
+
+      it('does not call .queue(...$factories)', () => {
+        expect(subject.queue).not.toHaveBeenCalled()
+      })
+    })
+
+    afterAll(() => {
+      _prototype.queue = _originalQueueMethod
+    })
   })
 
-  // TODO: Test sequential promise execution
+  describe('when instantiated', () => {
+    beforeEach(() => {
+      subject = new SequentialPromiseExecutor()
+    })
+
+    // TODO
+  })
 })
