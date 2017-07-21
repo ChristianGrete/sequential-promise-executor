fe5b1324f7716c2f9ea582677c7af165691fb64b
Christian Grete <webmaster@christiangrete.com>
2017-07-21 15:45:24 +0200

    chore(tasks): add prepublish task

--- tasks/release/index.js
+++ tasks/release/index.js
@@ -1,11 +1,13 @@
 import build from './build'
 import bump from './bump'
 import clean from './clean'
+import prepublish from './prepublish'
 import publish from './publish'
 
 export {
   build,
   bump,
   clean,
+  prepublish,
   publish
 }
