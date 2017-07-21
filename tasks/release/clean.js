9ad2bf9db4ad2c43c6a16b32404f1130cdaf60d7
Christian Grete <webmaster@christiangrete.com>
2017-07-21 23:39:27 +0200

    chore(tasks): refactor clean task

--- tasks/release/clean.js
+++ tasks/release/clean.js
@@ -1,6 +1,35 @@
+import {fork} from 'child_process'
+import {join} from 'path'
+
+const CLEAN_SCRIPT = join(__dirname, '../../scripts/clean.js')
+
 function clean(...$arguments) {
   return () => new Promise(($resolve, $reject) => {
-    $resolve()
+    let _hadCallback = false
+
+    fork(CLEAN_SCRIPT, $arguments)
+      .on('error', $error => {
+        if (_hadCallback) {
+          return
+        }
+
+        _hadCallback = true
+
+        $reject($error)
+      })
+      .on('exit', $code => {
+        if (_hadCallback) {
+          return
+        }
+
+        _hadCallback = true
+
+        if ($code === 0) {
+          $resolve()
+        } else {
+          $reject(new Error(`clean script exited with code ${$code}`))
+        }
+      })
   })
 }
 
