4cb190ba3e61b4fe2be2572824cb7fd9a56fe11f
Christian Grete <webmaster@christiangrete.com>
2017-07-21 14:31:02 +0200

    chore(scripts): update release tasks

--- scripts/release.js
+++ scripts/release.js
@@ -1,16 +1,31 @@
-import SequentialPromiseProcessor from '../src'
+import SequentialPromiseProcessor from '../src/'
+import {build, bump, clean} from '../tasks/release/'
+
+var version = 'patch'
+
+const ARGUMENTS = Object.freeze(process.argv.slice(2))
+
+const versions = /^(m(aj|in)or|p(atch|re(m(aj|in)or|patch|release)))$/
+
+ARGUMENTS.every($argument => {
+  if (versions.test($argument)) {
+    version = $argument
+    return false
+  }
+})
 
 const TASKS = Object.freeze([
+  clean(),
+  bump(version),
   // TODO ...
-  // runCleanScript(),
-  // bumpVersion('minor'),
   // git.add('--all'),
   // git.commit(`bump(version): ${version} [ci skip]`),
   // git.push(),
   // git.checkout('master'),
   // git.merge('develop'),
   // currentCommitHash = git.revParse('--verify HEAD'),
-  // runWebpack(),
+  build(),
+  // TODO ...
   // git.add('--all'),
   // git.commit(`release(artifact): ${version} [ci skip]`),
   // git.forcePush(),
@@ -19,7 +34,8 @@ const TASKS = Object.freeze([
   // optimizeManifests(),
   // publish(),
   // git.hardReset(currentCommitHash),
-  // runCleanScript('--artifact'),
+  clean('--artifact')
+  // TODO ...
   // git.forcePush(),
   // git.checkout('develop')
 ])
