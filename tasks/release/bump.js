b0564c4724d13f1c95c8f3eae7905a5730a76c2b
Christian Grete <webmaster@christiangrete.com>
2017-07-21 17:44:21 +0200

    chore(tasks): update bump task

--- tasks/release/bump.js
+++ tasks/release/bump.js
@@ -1,8 +1,12 @@
 import {exec} from 'child_process'
+import {readFile} from 'fs'
+import {join} from 'path'
 
-var version = 'patch'
+const PACKAGE = join(__dirname, '../../package.json')
 
 const versions = /^(m(aj|in)or|p(atch|re(m(aj|in)or|patch|release)))$/
+
+let version = 'patch'
 
 process.argv.slice(2).every($argument => {
   if (versions.test($argument)) {
@@ -11,11 +15,25 @@ process.argv.slice(2).every($argument => {
   }
 })
 
-function bump() {
+function bump($callback) {
   return () => new Promise(($resolve, $reject) => {
     exec(`npm version --no-git-tag-version ${version}`, $error => {
       if ($error === null) {
-        $resolve()
+        readFile(PACKAGE, 'utf8', ($error, $json) => {
+          var _version
+
+          if ($error === null) {
+            try {
+              $callback(JSON.parse($json).version)
+            } catch ($error) {
+              return $reject($error)
+            }
+
+            $resolve()
+          } else {
+            $reject($error)
+          }
+        })
       } else {
         $reject($error)
       }
