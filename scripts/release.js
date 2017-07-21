6b7e5bfd5fb91e9b8e9ac3ed8b61d0ab343ddf74
Christian Grete <webmaster@christiangrete.com>
2017-07-21 16:28:29 +0200

    chore(scripts): update release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,38 +1,25 @@
 import SequentialPromiseProcessor from '../src/'
-import {build, bump, clean, prepublish, publish} from '../tasks/release/'
-
-var version = 'patch'
-
-const ARGUMENTS = Object.freeze(process.argv.slice(2))
-
-const versions = /^(m(aj|in)or|p(atch|re(m(aj|in)or|patch|release)))$/
-
-ARGUMENTS.every($argument => {
-  if (versions.test($argument)) {
-    version = $argument
-    return false
-  }
-})
+import * as tasks from '../tasks/release/'
 
 const TASKS = Object.freeze([
-  // clean(),
-  // bump(version),
+  tasks.clean(),
+  tasks.bump(),
   // TODO: git.add('--all'),
   // TODO: git.commit(`bump(version): ${version} [ci skip]`),
   // TODO: git.push(),
   // TODO: git.checkout('master'),
   // TODO: git.merge('develop'),
   // TODO: currentCommitHash = git.revParse('--verify HEAD'),
-  // build(),
+  tasks.build(),
   // TODO: git.add('--all'),
   // TODO: git.commit(`release(artifact): ${version} [ci skip]`),
   // TODO: git.forcePush(),
   // TODO: git.tag(`v${version}`, 'Lorem ipsum dolor'),
   // TODO: git.pushTags(),
-  // prepublish(),
-  // publish(),
+  // tasks.prepublish(),
+  // tasks.publish(),
   // TODO: git.hardReset(currentCommitHash),
-  clean('artifact')
+  tasks.clean('artifact')
   // TODO: git.forcePush(),
   // TODO: git.checkout('develop')
 ])
