c886e92bd0235e09abf3c30fe2f492405359fb89
Christian Grete <webmaster@christiangrete.com>
2017-07-24 18:08:49 +0200

    chore(tasks): refactor bump task

--- tasks/release/bump.js
+++ tasks/release/bump.js
@@ -1,11 +1,47 @@
-// npm --no-git-tag-version version ${version}
-// git add --all
-// git commit -m "bump(version): $npm_package_version [ci skip]"
-// git push origin develop
+import {exec} from 'child_process'
 
-function bump() {
+const versions = /^(m(aj|in)or|p(atch|re(m(aj|in)or|patch|release)))$/
+
+let version = 'patch'
+
+process.argv.slice(2).every($argument => {
+  if (versions.test($argument)) {
+    version = $argument
+    return false
+  }
+})
+
+const run = ($reject, $command, $resolve) => {
+  try {
+    exec($command, ($error, $stdout) => {
+      if ($error === null) {
+        $resolve($stdout)
+      } else {
+        $reject($error)
+      }
+    })
+  } catch ($error) {
+    $reject($error)
+  }
+}
+
+function bump($setVersion) {
   return () => new Promise(($resolve, $reject) => {
-    $resolve()
+    const _run = run.bind(this, $reject)
+
+    _run(`npm version --no-git-tag-version ${version}`, $stdout => {
+      const _VERSION = $stdout.replace(/(?:^v|\r\n|\r|\n)/g, '')
+
+      $setVersion(_VERSION)
+
+      _run(
+        'git add --all',
+        () => _run(
+          `git commit -m 'bump(version): ${_VERSION} [ci skip]'`,
+          () => _run('git push origin develop', $resolve)
+        )
+      )
+    })
   })
 }
 
