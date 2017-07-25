31d06fc87b90b3e617dbf089fd78b02a9205387d
Christian Grete <webmaster@christiangrete.com>
2017-07-25 11:49:27 +0200

    chore(scripts): update distribute script

--- scripts/distribute.js
+++ scripts/distribute.js
@@ -2,14 +2,17 @@ import SequentialPromiseProcessor from '../src'
 import {build, bump, clean, finish, release, undo} from '../tasks/release'
 
 var commit
+var version
 
 const TASKS = Object.freeze([
   clean(),
-  bump(),
-  build($commit => {
+  bump($version => {
+    version = $version
+  }),
+  build(() => version, $commit => {
     commit = $commit
   }),
-  release(),
+  release(() => version),
   undo(() => commit),
   clean('distribution'),
   finish()
