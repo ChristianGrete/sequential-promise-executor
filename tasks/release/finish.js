1fa4c0fcbdf505af1473d5937eb79857f2a4a0b5
Christian Grete <webmaster@christiangrete.com>
2017-07-25 11:54:48 +0200

    chore(tasks): add finish task [wip]

--- tasks/release/finish.js
+++ tasks/release/finish.js
@@ -1,9 +1,27 @@
-// git push --force origin master
-// git checkout develop
+import {exec} from 'child_process'
+
+const run = ($reject, $command, $resolve) => {
+  try {
+    exec($command, ($error, $stdout) => {
+      if ($error === null) {
+        $resolve($stdout)
+      } else {
+        $reject($error)
+      }
+    })
+  } catch ($error) {
+    $reject($error)
+  }
+}
 
 function finish() {
   return () => new Promise(($resolve, $reject) => {
-    $resolve()
+    const _run = run.bind(this, $reject)
+
+    _run(
+      'echo "git push --force origin master"',
+      () => _run('echo "git checkout develop"', $resolve)
+    )
   })
 }
 
