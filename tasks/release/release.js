6c500f1f81474de540f3db54ff539bc5028e7b29
Christian Grete <webmaster@christiangrete.com>
2017-07-25 13:06:46 +0200

    chore(tasks): update release task

--- tasks/release/release.js
+++ tasks/release/release.js
@@ -1,11 +1,86 @@
-// git tag -a "v$npm_package_version" -m "release(?): lorem ipsum dolor"
-// git push --tags
-// prep package
-// npm publish --access public
+import {exec} from 'child_process'
+import {readFile, writeFile} from 'fs'
+import {join} from 'path'
 
-function release() {
+const MANIFEST_FILE = join(__dirname, '../../package.json')
+
+const messages = /^message=/
+
+let message = ''
+
+process.argv.slice(2).every($argument => {
+  if (messages.test($argument)) {
+    if (message.length > 0) {
+      message += ' '
+    }
+
+    message += $argument.replace(messages, '')
+    return false
+  }
+})
+
+const editFile = ($resolve, $reject) => () => {
+  readFile(MANIFEST_FILE, 'utf8', ($error, $json) => {
+    var _json
+    var _package
+
+    if ($error === null) {
+      _package = JSON.parse($json)
+
+      ;['devDependencies', 'scripts'].forEach($key => {
+        if (_package[$key] != null) {
+          _package[$key] = null
+        }
+
+        delete _package[$key]
+      })
+
+      if (_package.private) {
+        _package.private = false
+      }
+
+      _json = JSON.stringify(_package, null, 2)
+
+      writeFile(`${MANIFEST_FILE}-output.json`, _json, 'utf8', $error => {
+        if ($error === null) {
+          $resolve()
+        } else {
+          $reject($error)
+        }
+      })
+    } else {
+      $reject($error)
+    }
+  })
+}
+
+const run = ($reject, $command, $resolve) => () => {
+  exec($command, ($error, $stdout) => {
+    if ($error === null) {
+      $resolve($stdout)
+    } else {
+      $reject($error)
+    }
+  })
+}
+
+function release($getVersion) {
   return () => new Promise(($resolve, $reject) => {
-    $resolve()
+    try {
+      const _VERSION = $getVersion()
+
+      const _run = run.bind(this, $reject)
+
+      _run(
+        `echo "git tag -a 'v${_VERSION}' -m '${message}' && git push --tags"`,
+        editFile(
+          _run('echo "npm publish --access public"', $resolve),
+          $reject
+        )
+      )()
+    } catch ($error) {
+      $reject($error)
+    }
   })
 }
 
