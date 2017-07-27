c37f3f6f07e97badcc0bab59441a62c49347ac6f
Christian Grete <webmaster@christiangrete.com>
2017-07-27 11:01:18 +0200

    test(isThenable): add spec

--- src/isThenable.spec.js
+++ src/isThenable.spec.js
@@ -1,8 +1,77 @@
 import isThenable from './isThenable'
+import {createPromiseExecutor} from './__support__'
+
+const subject = isThenable
 
 describe('isThenable($value)', () => {
-  // TODO: Add actual tests - this is just to pass
-  it('is a function', () => {
-    expect(typeof isThenable).toBe('function')
+  describe('when invoked with promise as argument', () => {
+    const _promise = new Promise(createPromiseExecutor())
+
+    it('returns true', () => {
+      expect(subject(_promise)).toBe(true)
+    })
+  })
+
+  describe('when invoked with thenable as argument', () => {
+    var _thenable
+
+    describe('and thenable is object', () => {
+      _thenable = {
+        then() {}
+      }
+
+      it('returns true', () => {
+        expect(subject(_thenable)).toBe(true)
+      })
+    })
+
+    describe('and thenable is function', () => {
+      _thenable = () => {}
+      _thenable.then = () => {}
+
+      it('returns true', () => {
+        expect(subject(_thenable)).toBe(true)
+      })
+    })
+  })
+
+  describe('when invoked with non-thenable as argument', () => {
+    describe('and non-thenable is undefined', () => {
+      var _undefined
+
+      it('returns false', () => {
+        expect(subject(_undefined)).toBe(false)
+      })
+    })
+
+    describe('and non-thenable is null', () => {
+      it('returns false', () => {
+        expect(subject(null)).toBe(false)
+      })
+    })
+
+    describe('and non-thenable is primitive', () => {
+      it('returns false', () => {
+        expect(subject(0)).toBe(false)
+      })
+    })
+
+    describe('and non-thenable is function', () => {
+      it('returns false', () => {
+        expect(subject(() => {})).toBe(false)
+      })
+    })
+
+    describe('and non-thenable is object', () => {
+      it('returns false', () => {
+        expect(subject({})).toBe(false)
+      })
+    })
+  })
+
+  describe('when invoked with no argument', () => {
+    it('returns false', () => {
+      expect(subject()).toBe(false)
+    })
   })
 })
