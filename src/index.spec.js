a9a0ad917f8ba79a692c552d2bbf346e7bf9cd8f
Christian Grete <webmaster@christiangrete.com>
2017-07-27 11:01:02 +0200

    test(index): update spec

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,13 +1,23 @@
 import * as index from '.'
 
-describe('SequentialPromiseProcessor', () => {
-  describe('when imported', () => {
-    it('is available as named member', () => {
-      expect('SequentialPromiseProcessor' in index).toBe(true)
-    })
+describe('index', () => {
+  describe('.SequentialPromiseProcessor(...$factories)', () => {
+    describe('when imported', () => {
+      it('is available as named member', () => {
+        expect('SequentialPromiseProcessor' in index).toBe(true)
+      })
 
-    it('is also available as default member', () => {
-      expect(index.default === index.SequentialPromiseProcessor).toBe(true)
+      it('is also available as default member', () => {
+        expect(index.default === index.SequentialPromiseProcessor).toBe(true)
+      })
+    })
+  })
+
+  describe('.isThenable($value)', () => {
+    describe('when imported', () => {
+      it('is available as named member', () => {
+        expect('isThenable' in index).toBe(true)
+      })
     })
   })
 })
